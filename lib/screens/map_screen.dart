b41237b1fee3f6ea9e8e8ff60f0772c8f5a3ec87
nayeem-kazi <contact.nayeemkazi@gmail.com>
2023-01-14 13:03:42 +0600

    Google Map Added

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -1,4 +1,6 @@
+import 'package:app_with_map/widget/menu_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 import '../const/app_constant.dart';
 
@@ -10,6 +12,13 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen> {
+  late GoogleMapController mapController;
+
+  final LatLng _center = const LatLng(23.731272, 90.429631);
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,10 +28,13 @@ class _MapScreenState extends State<MapScreen> {
             "This is Home Page"
         ),
       ),
-      body: SafeArea(
-          child: SingleChildScrollView(
-            
-          )
+      drawer: NavDrware(),
+      body: GoogleMap(
+      onMapCreated: _onMapCreated,
+      initialCameraPosition: CameraPosition(
+        target: _center,
+        zoom: 14.0,
+      ),
       ),
     );
   }
---
+++ lib/widget/menu_bar.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class NavDrware extends StatefulWidget {
+  const NavDrware({Key? key}) : super(key: key);
+
+  @override
+  State<NavDrware> createState() => _NavDrwareState();
+}
+
+class _NavDrwareState extends State<NavDrware> {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+        // Add a ListView to the drawer. This ensures the user can scroll
+        // through the options in the drawer if there isn't enough vertical
+        // space to fit everything.
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text('Drawer Header'),
+            ),
+            ListTile(
+              title: const Text('Item 1'),
+              onTap: () {
+                Navigator.pop(context);
+                // Update the state of the app.
+                // ...
+              },
+            ),
+            ListTile(
+              title: const Text('Item 2'),
+              onTap: () {
+                Navigator.pop(context);
+                // Update the state of the app.
+                // ...
+              },
+            ),
+          ],
+        ),
+    );
+  }
+}
